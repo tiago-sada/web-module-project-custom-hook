a9b41920e8ae92127e58ff39a86d7499f677b98b
tiago-sada <tiago@sada.so>
2021-03-11 20:08:34 -0800

    Finished assignment

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import useLocalStorage from "./useLocalStorage"
 
-function useDarkMode() {
+function useDarkMode(initialValue) {
 
-    const [darkmode, setDarkmode] = useLocalStorage("isDarkmode", false)
+    const [darkmode, setDarkmode] = useLocalStorage("isDarkmode", (initialValue ? initialValue : false))
 
     return [darkmode, setDarkmode]
 
